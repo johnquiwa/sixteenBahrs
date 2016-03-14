cf84e83256118dae3d92566a8a71b7caefb306aa
johnquiwa <jcq211@nyu.edu>
2016-03-14 15:28:19 -0400

    change

--- server/main.js
+++ server/main.js
@@ -5,22 +5,11 @@ var chalk = require('chalk');
 // Create a node server instance! cOoL!
 var server = require('http').createServer();
 
-var createApplication = function () {
-    var app = require('./app');
-    server.on('request', app); // Attach the Express application.
-    require('./io')(server);   // Attach socket.io.
-};
+var app = require('./app');
+server.on('request', app); // Attach the Express application.
 
-var startServer = function () {
+var PORT = process.env.PORT || 1337;
 
-    var PORT = process.env.PORT || 1337;
-
-    server.listen(PORT, function () {
-        console.log(chalk.blue('Server started on port', chalk.magenta(PORT)));
-    });
-
-};
-
-createApplication()
-.then(startServer)
+server.listen(PORT, function () {
+    console.log(chalk.blue('Server started on port', chalk.magenta(PORT)));
 });
